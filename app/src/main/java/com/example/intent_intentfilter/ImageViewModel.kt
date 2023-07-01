d139065c27bf3f01ebc264a6f759d67f4ae962c1
Dipmala <telangdipmala@gmail.com>
2023-07-01 13:40:31 +0200

    Implicit Intent example - 1)email 2) google image ->share image to app

---
+++ app/src/main/java/com/example/intent_intentfilter/ImageViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.intent_intentfilter
+
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ImageViewModel: ViewModel() {
+
+    var uri: Uri? by mutableStateOf(null)
+        private set
+
+    fun updateUri(uri: Uri?){
+        this.uri = uri
+    }
+
+
+}
--- app/src/main/java/com/example/intent_intentfilter/MainActivity.kt
+++ app/src/main/java/com/example/intent_intentfilter/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.intent_intentfilter
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,9 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
 import com.example.intent_intentfilter.ui.theme.Intent_IntentFilterTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ImageViewModel> ()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,6 +34,9 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    viewModel.uri?.let{
+                        AsyncImage(model =viewModel.uri,contentDescription=null)
+                    }
                     Button(onClick = {
 
                         //explicit Intent example
@@ -58,6 +67,19 @@ class MainActivity : ComponentActivity() {
                         }
 
                         //.....
+
+
+// Implicit Intent....
+                       val intent = Intent(Intent.ACTION_SEND).apply {
+                            type ="text/plain"
+                            putExtra(Intent.EXTRA_EMAIL, arrayOf("test@test.com"))
+                            putExtra(Intent.EXTRA_SUBJECT,"This is my subject")
+                            putExtra(Intent.EXTRA_TEXT,"This is the Content of my email")
+                        }
+                        if (intent.resolveActivity(packageManager) != null)
+                        {
+                            startActivity(intent)
+                        }
                     }) {
                         Text(text = "Click Me ")
                     }
@@ -66,6 +88,17 @@ class MainActivity : ComponentActivity() {
 
             }
         }
+
+
+    }
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+        }
+        viewModel.updateUri(uri)
     }
 }
 
