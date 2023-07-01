bfaafa15b01746de98d4f09358f8a8f8c08f2790
Dipmala <telangdipmala@gmail.com>
2023-07-01 12:51:01 +0200

    Explicit Intent example

--- app/src/main/java/com/example/intent_intentfilter/MainActivity.kt
+++ app/src/main/java/com/example/intent_intentfilter/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.intent_intentfilter
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.intent_intentfilter.ui.theme.Intent_IntentFilterTheme
@@ -17,30 +23,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Intent_IntentFilterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+
+                        //explicit Intent example
+                        //TO GO one Activity to Another....
+
+                      /*  Intent(applicationContext, SecondActivity::class.java).also {
+                            startActivity(it)
+                        }*/
+
+                        //.............
+
+                        //Activity to another app eg youtube
+
+                        Intent(Intent.ACTION_MAIN).also {
+                            it.`package`="com.google.android.youtube"
+
+                            try {
+                                startActivity(it)
+                                } catch (e: ActivityNotFoundException){
+                                e.printStackTrace()
+                                 }
+
+                           /* if(it.resolveActivity(packageManager) != null){
+                                startActivity(it)
+                            }*/
+
+
+                        }
+
+                        //.....
+                    }) {
+                        Text(text = "Click Me ")
+                    }
+
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Intent_IntentFilterTheme {
-        Greeting("Android")
-    }
-}
+
+
